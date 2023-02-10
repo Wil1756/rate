3c1458fae9b85735a1e4e59cb159d3b77e4e5a72
John William <johnwilliams1756@gmail.com>
2023-02-10 13:27:42 +0300

    rating component

--- rate/src/components/Rating.tsx
+++ rate/src/components/Rating.tsx
@@ -3,11 +3,30 @@ import styles from './rating/Rating.module.css';
 
 export function Rating(){
     const [selectedRating, setSelectedRating] = useState<number>();
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     function handleRatingClicked(rating:number){
         setSelectedRating(rating);
     }
-    return <div className ={styles.panel}>
+
+    function handleFormSubmitted(e:React.FormEvent){
+        e.preventDefault();
+        setIsSubmitted(true);
+    }
+
+    return isSubmitted ? (
+        <div className={styles.thanks}><img className={styles.delivered} src='/business.jpg'/>
+            <p className={styles.selected}>You selected {selectedRating} out of 5</p>
+
+            <h1 className={styles.title}>Thank You!</h1>
+
+            <p className={styles.description}>We appreciate you taking the time to give a rating. If you ever need mpre support, don't hesitate to get in touch!</p>
+
+        </div>
+
+        
+    ):(
+    <form onSubmit={handleFormSubmitted}className ={styles.panel}>
         <img className={styles.star} src="/icons8-star.png"/>
 
         <h1 className={styles.title}>How did we do?</h1>
@@ -17,6 +36,7 @@ export function Rating(){
         <div className={styles.group}>
             {[1,2,3,4,5].map((rating)=>(
                 <button 
+                type='button'
                 onClick={()=> handleRatingClicked(rating)}
                 className={styles.rating}
                 >
@@ -25,7 +45,7 @@ export function Rating(){
             ))}
         </div>
 
-       <button className={styles.submit}>Submit</button>
-
-    </div>
+       <button disabled={selectedRating === undefined} className={styles.submit}>Submit</button>
+    </form>
+    )
 }
